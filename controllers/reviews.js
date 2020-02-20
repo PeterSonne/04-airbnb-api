b301cd70f345317c8c8f84247392a2994f7489d9
PeterSonne <papa@deep2chill.de>
2020-02-20 13:04:33 +0800

    Added GET in Reviews Controller

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -1,8 +1,11 @@
-const Reviews = require('../models/reviews')
-const router = require('express').Router()
+const Reviews = require("../models/reviews");
+const router = require("express").Router();
 
-router.get('/', (req, res) => {
-	// Respond with all reviews belonging to specific house and populate nested fields
-})
+router.get("/", (req, res) => {
+  // Respond with all reviews belonging to specific house and populate nested fields
+  Reviews.find(req.query)
+    .then(data => res.send(data))
+    .catch(err => console.log(err));
+});
 
-module.exports = router
+module.exports = router;

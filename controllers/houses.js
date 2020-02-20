e8468f293b42ede53bbc31f118881a215a2786b3
PeterSonne <papa@deep2chill.de>
2020-02-20 13:30:31 +0800

    Added Population for houses, reviews

--- controllers/houses.js
+++ controllers/houses.js
@@ -1,8 +1,11 @@
 const Houses = require("../models/houses");
 const router = require("express").Router();
+require("../models/users.js");
+require("../models/amenities.js");
 
 router.get("/", (req, res) => {
   Houses.find(req.query)
+    .populate(["type", "host", "amenities"])
     .then(houses => {
       // Set first of images as 'image'
       res.send(houses);
@@ -10,6 +13,13 @@ router.get("/", (req, res) => {
     .catch(err => {
       res.send(err);
     });
+});
+
+router.get("/:id", (req, res) => {
+  Houses.findById(req.params.id)
+    .populate(["type", "host", "amenities"])
+    .then(data => res.send(data))
+    .catch(err => console.log(err));
 });
 
 // Create route for single house
--- controllers/reviews.js
+++ controllers/reviews.js
@@ -1,9 +1,12 @@
 const Reviews = require("../models/reviews");
 const router = require("express").Router();
+require("../models/users.js");
+require("../models/houses.js");
 
 router.get("/", (req, res) => {
   // Respond with all reviews belonging to specific house and populate nested fields
   Reviews.find(req.query)
+    .populate(["author", "house"])
     .then(data => res.send(data))
     .catch(err => console.log(err));
 });
